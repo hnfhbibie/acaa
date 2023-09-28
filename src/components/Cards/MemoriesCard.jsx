9e43c4ccd475cb0670598ca0f65bbdc08432968d
hnfhbibie <hanifhabibie1@gmail.com>
2023-09-28 20:52:31 +0700

    add revition

--- src/components/Cards/MemoriesCard.jsx
+++ src/components/Cards/MemoriesCard.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const MemoriesCard = ({ imgUrl, alt, headline, body }) => {
   return (
-    <div className="flex items-center justify-between rounded-xl bg-underline shadow-xl w-[300px] h-[150px] lg:w-[400px] lg:h-[250px]">
-      <div className="w-full h-full">
+    <div className="flex items-center rounded-xl bg-underline shadow-xl w-[300px] h-[150px] lg:w-[400px] lg:h-[250px]">
+      <div className="h-full">
         <img
           src={imgUrl}
           alt={alt}
-          className="object-cover w-full h-full rounded-lg"
+          className="object-cover h-full max-w-lg rounded-lg"
         />
       </div>
-      <div className="flex flex-col items-center justify-center gap-5 max-w-md mr-2 my-2 ">
+      <div className="flex flex-col items-center justify-center max-w-md gap-2 px-2 py-2">
         <h2 className="text-center  font-poppin text-sm font-semibold">
           {headline}
         </h2>
-        <p className="text-start font-nunito font-thin text-sm ">{body}</p>
+        <p className="text-start font-nunito font-thin text-[10px] max-w-sm">
+          {body}
+        </p>
       </div>
     </div>
   );
